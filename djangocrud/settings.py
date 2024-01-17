8a1f2a7c8ce7c318ae8e351dc7c5e9ba17776ac3
panchito9000 <panchito9000@example.com>
2024-01-16 21:52:36 -0500

    deploy cpd

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenonise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
